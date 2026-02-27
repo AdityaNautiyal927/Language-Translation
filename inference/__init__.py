ee77e241065516d09d9ba0cc7d876adc48b57fc6
Aditya Nautiyal <adityanautiya927@gmail.com>
2026-02-27 19:34:42 +0530

    updated the requirements

--- inference/__init__.py
+++
@@ -1,4 +0,0 @@
-# inference/__init__.py
-from inference.translator import Translator, ModelRegistry, ModelNotFoundError, TranslationResult
-
-__all__ = ["Translator", "ModelRegistry", "ModelNotFoundError", "TranslationResult"]
